22e28590d60e0f6af1ad4e343329e931b50ba244
Murugan Pandian <murugan@telecmi.com>
2026-04-10 16:02:21 +0530

    feat: add teamTransfer, fix getCallId return types, and bump version to 0.14.0

--- src/socket.js
+++ src/socket.js
@@ -3,12 +3,12 @@ import io from 'socket.io-client';
 
 class SocketCMI {
 
-    constructor( token, _this ) {
+    constructor(token, _this) {
 
 
-        this.socket = io( 'https://notify.telecmi.com', {
+        this.socket = io('https://notify.telecmi.com', {
             query: { token: token }
-        } );
+        });
 
 
 
@@ -17,64 +17,80 @@ class SocketCMI {
 
 
 
-        this.socket.on( 'disconnect', ( reson ) => {
+        this.socket.on('disconnect', (reson) => {
             _this.ready_transfer = false;
-            if ( reson == 'transport close' ) {
-                _this.emit( 'net_changed', { code: 400, msg: 'network changed' } )
+            if (reson == 'transport close') {
+                _this.emit('net_changed', { code: 400, msg: 'network changed' })
             }
-        } );
+        });
 
-        this.socket.on( "connect_error", () => {
+        this.socket.on("connect_error", () => {
 
             _this.ready_transfer = false;
-        } );
+        });
 
-        this.socket.on( 'connect', () => {
+        this.socket.on('connect', () => {
 
             _this.ready_transfer = true;
-            this.socket.emit( 'wssip-agent-opt', { join: true } )
-        } );
+            this.socket.emit('wssip-agent-opt', { join: true })
+        });
 
-        this.socket.on( 'cmi_transfer', ( data ) => {
+        this.socket.on('cmi_transfer', (data) => {
 
-            if ( data.state == 'init' ) {
-                _this.sendDtmf( "*9" );
+            if (data.state == 'init') {
+                _this.sendDtmf("*9");
             }
-            _this.emit( 'transfer', data )
-        } );
+            _this.emit('transfer', data)
+        });
 
-        this.socket.on( 'force_logout', ( data ) => {
+        this.socket.on('force_logout', (data) => {
 
             _this.logout();
-            _this.emit( 'sbc_logout', data );
-        } );
+            _this.emit('sbc_logout', data);
+        });
 
-        this.socket.on( 'cmi_record', ( data ) => {
+        this.socket.on('cmi_record', (data) => {
 
-            _this.emit( 'record', data )
-        } );
+            _this.emit('record', data)
+        });
 
 
 
     }
 
-    transfer ( uuid, to, callback ) {
+    transfer(uuid, to, callback) {
 
 
-        if ( this.socket.connected ) {
-            this.socket.emit( 'agent-call-transfer', { uuid: uuid, to: to }, ( data ) => {
-                if ( typeof callback === 'function' ) {
-                    callback( data )
+        if (this.socket.connected) {
+            this.socket.emit('agent-call-transfer', { uuid: uuid, to: to }, (data) => {
+                if (typeof callback === 'function') {
+                    callback(data)
                 }
-            } )
-        } else if ( typeof callback === 'function' ) {
-            callback( { error: "Socket is not connected" } )
+            })
+        } else if (typeof callback === 'function') {
+            callback({ error: "Socket is not connected" })
         }
 
     }
 
-    cancel ( uuid ) {
-        this.socket.emit( 'agent-cancel-transfer', { uuid: uuid } )
+
+    teamTransfer(uuid, to, callback) {
+
+
+        if (this.socket.connected) {
+            this.socket.emit('team-call-transfer', { uuid: uuid, to: to }, (data) => {
+                if (typeof callback === 'function') {
+                    callback(data)
+                }
+            })
+        } else if (typeof callback === 'function') {
+            callback({ error: "Socket is not connected" })
+        }
+
+    }
+
+    cancel(uuid) {
+        this.socket.emit('agent-cancel-transfer', { uuid: uuid })
     }
 
 
